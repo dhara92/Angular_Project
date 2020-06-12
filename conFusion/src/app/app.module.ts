de5876e20efd9da77c50c480ddff9bc484618e83
dharanarola2012@gmail.com <dharanarola2012@gmail.com>
2020-06-12 17:59:24 -0400

    databinding

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -3,21 +3,29 @@ import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/Toolbar';
 import {FlexLayoutModule} from '@angular/flex-layout';
-
+import {MatListModule} from '@angular/material/list';
 import { AppComponent } from './app.component';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatListModule,MatGridListModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
