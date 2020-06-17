925af6ad868967de44a8a67aeb021fe8d339bccb
dharanarola2012@gmail.com <dharanarola2012@gmail.com>
2020-06-17 12:09:54 -0400

    Single Page Application

--- conFusion/src/app/app-routing/routes.ts
+++ conFusion/src/app/app-routing/routes.ts
@@ -9,6 +9,7 @@ import { DishdetailComponent } from '../dishdetail/dishdetail.component';
 export const routes: Routes =[
     { path: 'home', component: HomeComponent },
     { path: 'menu', component: MenuComponent },
+    { path: 'dishdetail/:id', component: DishdetailComponent },
     { path: 'about', component: AboutComponent },
     { path: 'contactus', component: ContactComponent },
     { path: '', redirectTo: '/home', pathMatch: 'full'}
